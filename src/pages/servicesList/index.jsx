f38876f1e9182159b60cadf8de3c7a2bacc4a53a
Eliabe Santos <eliabedodo@gmail.com>
2021-01-13 16:44:07 -0300

    service page preparada para receber service card

--- src/pages/servicesList/index.jsx
+++ src/pages/servicesList/index.jsx
@@ -1,7 +1,17 @@
 import MainIMage from "./serviceImg.svg";
 import Header from "../../components/header";
 
-import { MainContainer, ServiceImageContainer, ServiceImage } from "./style";
+import {
+  MainContainer,
+  ServiceImageContainer,
+  ServiceImage,
+  ServiceInfo,
+  Title,
+  Rate,
+  Description,
+} from "./style";
+
+import { BsStarFill } from "react-icons/bs";
 
 const ServiceList = () => {
   return (
@@ -11,6 +21,13 @@ const ServiceList = () => {
         <ServiceImageContainer>
           <ServiceImage src={MainIMage}></ServiceImage>
         </ServiceImageContainer>
+        <ServiceInfo>
+          <Title>Lavanderia 2 irmaos</Title>
+          <Rate>
+            4,5 <BsStarFill color="yellow" size="20px" />
+          </Rate>
+          <Description>Pedido minimo de 10 itens</Description>
+        </ServiceInfo>
       </MainContainer>
     </>
   );
--- src/pages/servicesList/style.js
+++ src/pages/servicesList/style.js
@@ -4,13 +4,42 @@ export const MainContainer = styled.div`
   width: 100%;
   height: 90vh;
   background-color: #ffffff;
+  display: flex;
+  flex-direction: column;
 `;
 export const ServiceImageContainer = styled.div`
   width: 100%;
-  height: 5rem;
+  height: 8rem;
 `;
 export const ServiceImage = styled.img`
   width: 100%;
+  height: 8rem;
   margin: 5px 0;
   filter: blur(5px);
+`;
+export const ServiceInfo = styled.div`
+  width: 100%;
+  height: 10rem;
+
+  display: flex;
+  flex-wrap: wrap;
+`;
+export const Title = styled.h1`
+  width: 69%;
+  height: 4rem;
+  font-weight: normal;
+  text-align: left;
+  padding-left: 0.5rem;
+`;
+export const Rate = styled.h2`
+  width: 29%;
+  height: 4rem;
+  font-weight: normal;
+`;
+export const Description = styled.h1`
+  width: 100%;
+  height: 4rem;
+  font-size: 16px;
+  font-weight: normal;
+  text-align: center;
 `;
