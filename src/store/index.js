b11d70d017667606915ea24a445cacb2dfc596c5
kevin wischneski <kevinwischneski@gmail.com>
2021-01-18 12:43:26 -0300

    feat: Adicionando helper peara validaçao da autenticaçao do token, ajustes no thunk e adiçao do dispatch na app.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import { addToCartReducer } from "./modules/cart/reducer";
 import { laundriesReducer } from "./modules/laundries/reducer";
-import { userReducer } from "./modules/user/reducer";
+import { userReducer } from "./modules/currentUser/reducer";
 
 const reducers = combineReducers({
   cart: addToCartReducer,
--- src/store/modules/currentUser/thunk.js
+++ src/store/modules/currentUser/thunk.js
@@ -1,21 +1,23 @@
 import axios from "axios";
 import jwt_decode from "jwt-decode";
-
+import { tokenValidator } from "../../../helpers/tokenValidator";
 import { getUser } from "./actions";
 
-export const getAllThunk = () => async (dispatch, _) => {
+export const getUserThunk = () => async (dispatch, _) => {
+  if (!tokenValidator()) {
+    return;
+  }
   try {
     const res = await axios.get("https://easy-wash-server.herokuapp.com/users");
     const users = await res.data;
     const token = localStorage.getItem("authToken");
-    const decoded = jwt_decode(token);
-    const userId = decoded.sub;
-    const loggedUser = users.filter((user) => {
-      return user.id === userId;
-    });
+    const decoded = await jwt_decode(token);
+    const userId = await decoded.sub;
+    const loggedUser = await users.filter((user) => {
+      return user.id === parseInt(userId);
+    })[0];
     dispatch(getUser(loggedUser));
   } catch (err) {
     console.log(err);
-    dispatch(getUser({}));
   }
 };
