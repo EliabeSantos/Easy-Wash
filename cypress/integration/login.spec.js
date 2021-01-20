9f2db384aeb430eaf1ed1b17e204cb96287346e3
DiegoPires <programadorpires@gmail.com>
2021-01-20 15:29:53 -0300

    feat:Feito o teste de login cypress

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,16 +1,28 @@
 context("Login Page", ()=>{
   it('logs in with faild credentials',()=>{
+
+
     cy.viewport(1440,900)
     cy.visit('http://localhost:3000/login')
 
     cy.contains('Email')
     cy.contains('Senha')
     cy.contains('Entrar')
-    cy.get('.sc-kstrdz > .MuiButtonBase-root').contains('Entrar')
+    
+    cy.get('.MuiButtonBase-root').contains('Entrar')
 
-    cy.get(':nth-child(1) > .MuiInputBase-root > .MuiInputBase-input').type('carro@homail.com')
+    cy.get(':nth-child(1) > .MuiInputBase-root > .MuiInputBase-input').type('edimara@hotmail.com')
     cy.get(':nth-child(2) > .MuiInputBase-root > .MuiInputBase-input').type('1234')
-    cy.get('.sc-kstrdz > .MuiButtonBase-root > .MuiButton-label').click();
-    cy.contains('Senha ou usuário inválido')
+    
+    cy.server();
+    cy.route({
+          method: "POST",
+          url: '/login'
+    }).as("login");
+
+    cy.get('.MuiButtonBase-root > .MuiButton-label').last().click();
+
+    cy.contains('Joao matos de almeida, 458 - Vila Zumbi dos Palmares, colombo')
+    
   })
 })
