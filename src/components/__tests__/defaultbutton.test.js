86e180616dc55a58444cb8ab7f9ada3c0a11996d
Eliabe Santos <eliabedodo@gmail.com>
2021-01-21 10:29:24 -0300

    teste

--- src/components/__tests__/defaultbutton.test.js
+++ src/components/__tests__/defaultbutton.test.js
@@ -1,18 +1,15 @@
-import {render,screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import DefaultButton from "../sharedButton";
 import "@testing-library/jest-dom";
 
-describe("Quando tiver tudo ok",()=>{
-  test("Verificando se componente botão esta sendo feito",()=>{
-      render(<DefaultButton name="Enviar"></DefaultButton>);      
-      expect(
-        screen.getByRole('button',{name:/Enviar/i})
-      ).toBeInTheDocument();
+describe("Quando tiver tudo ok", () => {
+  test("Verificando se componente botão esta sendo feito", () => {
+    render(<DefaultButton name="Enviar"></DefaultButton>);
+    expect(screen.getByRole("button", { name: /Enviar/i })).toBeInTheDocument();
   });
 
-test("Não esta certo",()=>{
-  render(<DefaultButton name="edimara1"></DefaultButton>);
-  expect(screen.queryByText("edimara")).not.toBeInTheDocument();
-})
-
+  test("Não esta certo", () => {
+    render(<DefaultButton name="edimara1"></DefaultButton>);
+    expect(screen.queryByText("edimara")).not.toBeInTheDocument();
+  });
 });
